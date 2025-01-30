af71888ce01bfafedd6b32bf612cf9a00a37dd55
Your Name <you@example.com>
2025-01-30 22:20:48 +0200

    valmis

---
+++ kujundid.py
@@ -0,0 +1,42 @@
+import turtle
+suurus = 100
+t = turtle.Turtle()
+t.speed(3)
+t.pensize(3)
+
+t.penup()
+t.goto(-suurus / 2, 0)
+t.pendown()
+for i in range(4):
+    t.forward(suurus)
+    t.left(90)
+
+t.penup()
+t.pencolor("red")
+t.goto(-suurus / 50, 50)
+t.pendown()
+for i in range(4):
+    t.forward(suurus)
+    t.left(90)
+
+
+t.penup()
+t.pencolor("black")
+t.goto(-suurus / 2, -200)
+t.pendown()
+for i in range(4):
+    t.forward(suurus)
+    t.left(90)
+
+t.penup()
+t.pencolor("red")
+t.goto(-suurus / -100, -200)
+t.pendown()
+for i in range(4):
+    t.left(90)
+    t.forward(suurus)
+    
+
+
+t.hideturtle()
+turtle.done()
