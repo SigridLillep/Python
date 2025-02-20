73da152ae1dc66beb66a32e92ee7614c761766a0
Your Name <you@example.com>
2025-02-20 10:28:56 +0200

    pooleli

--- Kodutoo3.py
+++ Kodutoo3.py
@@ -25,13 +25,21 @@ with open('users.json', 'r', encoding='utf-8') as file:
         macAddress = user['macAddress']
         university = user['university']
         bank = f"{user['bank']['cardExpire']}, {user['bank']['cardNumber']}, {user['bank']['cardType']}, {user['bank']['currency']}, {user['bank']['iban']}"
-
+        company = f"{user['company']['department']}, {user['company']['name']}, {user['company']['title']}"
+        company_address = f"{user['company']['address']['address']}, {user['company']['address']['city']}, {user['company']['address']['state']}, {user['company']['address']['stateCode']}, {user['company']['address']['postalCode']}, {user['company']['address']['coordinates']['lat']}, {user['company']['address']['coordinates']['lng']}, {user['company']['address']['country']}"
+        ein = user['ein']
+        ssn = user['ssn']
+        userAgent = user['userAgent']
+        crypto = f"{user['crypto']['coin']}, {user['crypto']['wallet']}, {user['crypto']['network']}"
+        role = user['role']
 
 
         print(f"Name: {name}")
         print(f"Hair: {hair}")
         print(f"Address: {address}")
         print(f"Bank info: {bank}")
+        print(f"Company info: {company}, \nCompany address: {company_address}")
+        print(f"Crypto info: {crypto}")
 
     # for user in users: 
     #     kasutaja_valik = int(input("Vali kasutaja ID järgi (1-30):"))
